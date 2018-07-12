7877a71addff9e8a4cb4ea2d0c502f7051ff1f9e
nachtmusik <to.akiyama.dev@gmail.com>
2018-07-12 14:22:06 +0900

    User <-> Meeting の関係付ルール追加

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,2 +1,5 @@
 class Meeting < ApplicationRecord
+  belongs_to :user
+  belongs_to :inspector, class_name: 'User'
+  belongs_to :authorither, class_name: 'User'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :responsible_meetings, class_name: 'Meeting', foreign_key: 'user_id'
+  has_many :inspect_meetings,     class_name: 'Meeting', foreign_key: 'inspecter_id'
+  has_many :approve_meetings,     class_name: 'Meeting', foreign_key: 'authorither_id'
 end
