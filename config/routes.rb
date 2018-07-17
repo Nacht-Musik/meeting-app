70a28581d385e916c6d3c8dad7998b5b4a05cdb3
nachtmusik <to.akiyama.dev@gmail.com>
2018-07-17 16:35:31 +0900

    'My'Controller & 'page' View追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'top_pages#index'
 
+  # devise (User) 関連
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
@@ -10,5 +11,9 @@ Rails.application.routes.draw do
     :registrations => 'users/registrations'
   }
 
+
+  # 'My'関連
+  get 'my/page'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
