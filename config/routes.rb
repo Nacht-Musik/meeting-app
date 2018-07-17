79c4dfd943e70aeda8d95f52e59b7bcb49f35110
nachtmusik <to.akiyama.dev@gmail.com>
2018-07-17 16:42:31 +0900

    'my/meeting'View追加

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
 
   # 'My'関連
   get 'my/page'
-  
+  get 'my/meeting'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
