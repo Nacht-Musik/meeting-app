1387d0d42ca0e13baa7a0d730f72308c0d0442db
nachtmusik <to.akiyama.dev@gmail.com>
2018-07-19 00:52:51 +0900

    詳細ページの雛形作成

--- app/controllers/meeting_controller.rb
+++ app/controllers/meeting_controller.rb
@@ -1,4 +1,6 @@
 class MeetingController < ApplicationController
+  before_action :set_meeting, only: [:show]
+
   def show
   end
 
@@ -7,6 +9,7 @@ class MeetingController < ApplicationController
       @meeting = Meeting.find(params[:id])
     end
 
+    # Strong parameters設定
     # def meeting_params
     #   params.require(:meeting).permit(:title, :user_id, :date)
     # end
