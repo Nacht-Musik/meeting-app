d4747653a2a7763b4c7910d58183a5e3580d38a1
nachtmusik <to.akiyama.dev@gmail.com>
2018-07-18 16:27:21 +0900

    会議録ステータス毎にタブ化。bootstrapのjs設定追加

--- app/controllers/my_controller.rb
+++ app/controllers/my_controller.rb
@@ -1,7 +1,15 @@
 class MyController < ApplicationController
+  before_action :set_meeting, only: [:meeting]
+
   def page
   end
 
   def meeting
   end
+
+
+  private
+    def set_meeting
+      @meetings = Meeting.all
+    end
 end
