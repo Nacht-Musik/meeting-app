13d87da9e1cd5e9d22b4227d789fcec6a12a7bc3
nachtmusik <to.akiyama.dev@gmail.com>
2018-07-19 00:25:44 +0900

    会議録詳細Viewのルーティング設定追加

--- app/controllers/meeting_controller.rb
+++ app/controllers/meeting_controller.rb
@@ -1,2 +1,13 @@
 class MeetingController < ApplicationController
+  def show
+  end
+
+  private
+    def set_meeting
+      @meeting = Meeting.find(params[:id])
+    end
+
+    # def meeting_params
+    #   params.require(:meeting).permit(:title, :user_id, :date)
+    # end
 end
