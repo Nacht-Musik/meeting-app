56ab9b0ae3c8b0bd1369fec8722ededcd2c5ef70
nachtmusik <to.akiyama.dev@gmail.com>
2018-07-19 16:56:23 +0900

    Topicをソート番号順に並び替え

--- app/controllers/meeting_controller.rb
+++ app/controllers/meeting_controller.rb
@@ -2,6 +2,10 @@ class MeetingController < ApplicationController
   before_action :set_meeting, only: [:show]
 
   def show
+    # ソート番号順に並んだTopicsを取得
+    @topics = sort_topics(@meeting)
+    # トップのコメントを全て取得
+    # @fountder_comments = ""
   end
 
   private
@@ -9,7 +13,13 @@ class MeetingController < ApplicationController
       @meeting = Meeting.find(params[:id])
     end
 
-    # Strong parameters設定
+    def sort_topics (meeting)
+      meeting.topics.sort do |a, b|
+        a.sort_num <=> b.sort_num
+      end
+    end
+
+    # Strong parameters
     # def meeting_params
     #   params.require(:meeting).permit(:title, :user_id, :date)
     # end
