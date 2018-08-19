287addc9754c9781a3b63b51ae2fb671bc4294b3
nachtmusik <to.akiyama.dev@gmail.com>
2018-08-19 20:21:53 +0900

    MeetingとProjectのリレーション追加

--- app/models/family_comment.rb
+++
@@ -1,7 +0,0 @@
-class FamilyComment < ApplicationRecord
-  belongs_to :comment
-  belongs_to :child, class_name: 'Comment'
-
-  validates :comment_id, presence: true
-  validates :child_id, presence: true
-end
--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -2,6 +2,7 @@ class Meeting < ApplicationRecord
   belongs_to :user
   belongs_to :inspector, class_name: 'User'
   belongs_to :approver, class_name: 'User'
+  belongs_to :project
 
   has_many :topics, class_name: 'Topic'
   accepts_nested_attributes_for :topics, allow_destroy: true, reject_if: :all_blank
