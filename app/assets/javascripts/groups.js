4eb0019b98efeb3b67a3ac35ef48c317df50e0d4
nachtmusik <to.akiyama.dev@gmail.com>
2018-09-13 23:11:13 +0900

    グループメンバー追加ボタン・削除ボタンを追加

--- app/assets/javascripts/groups.js
+++ app/assets/javascripts/groups.js
@@ -2,20 +2,11 @@ $(document).on('turbolinks:load', function() {
   // メンバー追加ボタン
   $('#group-page').on('click', '#member-add-btn', function () {
     console.log("メンバー追加ボタン押下");
-    /*
-      1. セレクターで選択されているユーザー情報を取得
-      2. member-blockを生成する
-      3. member-add-selector に対象ユーザーを追加する
-    */
+
     // 追加するユーザー情報取得
     let user_id = $('#member-selector option:selected').val();
-    let user_name = $('#member-selector option:selected').text();
     let authority = $('#authority-selector option:selected').val();
-    let authority_text = $('#authority-selector option:selected').text();
     let admin_flag = $('#admin-selector option:selected').val();
-    let admin_text = $('#admin-selector option:selected').text();
-    // console.log({user_id: user_id, authority: authority, admin: admin_flag});
-    // console.log({name: user_name, authority: authority_text, admin: admin_text});
 
     // DB登録用のid/nameを生成
     let id_com = "group_group_members_attributes";
@@ -25,7 +16,6 @@ $(document).on('turbolinks:load', function() {
     let model_num = new Date().getTime().toString(10);
     id_com += "_" + model_num;
     name_com += "[" + model_num.toString(10) + "]";
-    // console.log({id: id_com, name: name_com});
 
 
     // 追加ユーザーのGroupMember Model登録用データ作成
@@ -49,20 +39,36 @@ $(document).on('turbolinks:load', function() {
     member_params_ele.find('.member-destroy-flag').val('false');
     member_params_ele.find('.member-destroy-flag').attr('id', id_com + "__destroy");
     member_params_ele.find('.member-destroy-flag').attr('name', name_com + "[_destroy]");
-    console.log(member_params_ele.find('.member-user-id'));
+
 
     $('#member-params-area').append(member_params_ele);
 
 
-    // 追加ユーザーの表示用データ作成
-    let member_row_ele = $('#member-block-template').find('.member-row');
+    // 追加ユーザーの表示用データ取得
+    let first_name = $('#member-selector option:selected').attr('data-first-name');
+    let last_name = $('#member-selector option:selected').attr('data-last-name');
+    let user_name = last_name + " " + first_name;
+    let email = $('#member-selector option:selected').attr('data-email');
+    let authority_text = $('#authority-selector option:selected').text();
+    let admin_text = $('#admin-selector').attr('data-mark');
+
+
+    // 追加メンバー表示データ展開
+    let member_row_ele = $('#member-block-template').find('.member-row').clone();
     member_row_ele.find('.user-name').text(user_name);
+    member_row_ele.find('.user-email').text(email);
     member_row_ele.find('.user-authority').text(authority_text);
-    member_row_ele.find('.user-admin').text(admin_text);
+    member_row_ele.find('.member-edit-btn').attr('data-user-id', user_id);
+    member_row_ele.find('.member-del-btn').attr('data-user-id', user_id);
+
+    if(admin_flag === "1"){
+      member_row_ele.find('.user-admin').append(admin_text);
+    }
+
     $('#member-list-area').append(member_row_ele);
 
+    // 追加したユーザーをセレクターから削除
 
-    // 追加ユーザーをセレクターから削除
   })
 
   // メンバー削除ボタン
@@ -74,12 +80,28 @@ $(document).on('turbolinks:load', function() {
       2. 対象ユーザーの_destroy flagを立てる
       3. member-add-selector に対象ユーザーを追加する
     */
-    $(this).parents()
+
+    // 対象ユーザーのID番号を取得
+    let user_id = $(this).attr('data-user-id');
+
+    // 対象メンバーの削除フラグ(_destroy)をTrueにする
+    let user_params_elems = $('#member-params-area').find('.member-params');
+    user_params_elems.each(function(){
+      if($(this).attr('data-user-id') == user_id){
+        $(this).find('.member-destroy-flag').val('true');
+      }
+    });
+
+    // 対象メンバーの表示部を削除
+    let user_row_ele = $(this).closest('.member-row');
+    user_row_ele.remove();
+
+    // メンバー追加セレクターに削除したユーザーを追加する
 
 
-  })
+  });
 
-})
+});
 
 
 // メンバーセレクターの初期化
