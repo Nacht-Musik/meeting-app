2d7b84103c1e04556b1e7c15c989c7eb87cfb043
nachtmusik <to.akiyama.dev@gmail.com>
2018-09-14 00:17:41 +0900

    メンバーの多重登録対策（追加済みメンバーをセレクターから除外）

--- app/assets/javascripts/groups.js
+++ app/assets/javascripts/groups.js
@@ -3,6 +3,14 @@ $(document).on('turbolinks:load', function() {
   $('#group-page').on('click', '#member-add-btn', function () {
     console.log("メンバー追加ボタン押下");
 
+    // セレクターで選択されているユーザー名を取得
+    let selected_user_name = $('#member-selector option:selected').text();
+
+    // 選択ユーザー名が空の場合、追加処理を実行しない
+    if(selected_user_name === ""){
+      return;
+    }
+
     // 追加するユーザー情報取得
     let user_id = $('#member-selector option:selected').val();
     let authority = $('#authority-selector option:selected').val();
@@ -68,18 +76,18 @@ $(document).on('turbolinks:load', function() {
     $('#member-list-area').append(member_row_ele);
 
     // 追加したユーザーをセレクターから削除
+    $('#member-selector option:selected').remove();
 
+    // セレクターの中身が空になったら、追加ボタンを無効にする
+    let option_num = $('#member-selector').children('option').length;
+    if(option_num <= 0){
+      $(this).addClass("disabled");
+    }
   })
 
   // メンバー削除ボタン
   $('#group-page').on('click', '.member-del-btn', function () {
     console.log("メンバー削除");
-    /*
-      0. 削除確認メッセージを表示
-      1. 表示リストから対象ユーザーブロックを削除
-      2. 対象ユーザーの_destroy flagを立てる
-      3. member-add-selector に対象ユーザーを追加する
-    */
 
     // 対象ユーザーのID番号を取得
     let user_id = $(this).attr('data-user-id');
@@ -92,13 +100,29 @@ $(document).on('turbolinks:load', function() {
       }
     });
 
-    // 対象メンバーの表示部を削除
+    // 対象メンバーのユーザー情報を取得
     let user_row_ele = $(this).closest('.member-row');
-    user_row_ele.remove();
+    let user_name = user_row_ele.find('.user-name').text();
+    let user_name_split = user_name.split(' ');
+    let last_name = user_name_split[0].replace(/\s+/g, "");
+    let first_name = user_name_split[1].replace(/\s+/g, "");
+    let email = user_row_ele.find('.user-email').text().replace(/\s+/g, "");
 
     // メンバー追加セレクターに削除したユーザーを追加する
+    var add_option_attr = {value: user_id, text: user_name + "（" + email + "）",
+                            'data-last-name': last_name,
+                            'data-first-name': first_name,
+                            'data-email': email};
+    var add_option = $('<option>', add_option_attr);
+    $('#member-selector').append(add_option);
 
+    // 対象メンバーの表示部を削除
+    user_row_ele.remove();
 
+    // メンバー追加ボタンが無効になっていたら有効にする。
+    if ($('#member-add-btn').hasClass('disabled')) {
+      $('#member-add-btn').removeClass("disabled");
+    }
   });
 
 });
@@ -106,10 +130,15 @@ $(document).on('turbolinks:load', function() {
 
 // メンバーセレクターの初期化
 function initializationMemberSelector() {
-  /*
-    1. グループメンバーのuser_idを全て取得する
-    2. memberselector 内のoption.val()とmember.user_idが一致したoptionを削除する
-   */
-
+  // 所属済みのメンバーを追加セレクターから削除
+  let member_ids = $('#member-params-area').find('.member-user-id');
+  member_ids.each(function(i, user_id){
+    $('#member-selector option[value=' + user_id.value + ']').remove();
+  });
+  // メンバー追加セレクターが空だったら、追加ボタンを無効にする
+  let member_option_num = $('#member-selector').children('option').length;
+  if(member_option_num <= 0){
+    $('#member-add-btn').addClass("disabled");
+  }
 }
 
