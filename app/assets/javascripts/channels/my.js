e1630daac3e01491fd467fe60de63bccdb98a85a
nachtmusik <to.akiyama.dev@gmail.com>
2018-09-29 00:17:16 +0900

    my_pageのデザイン変更

---
+++ app/assets/javascripts/channels/my.js
@@ -0,0 +1,10 @@
+$(document).on('turbolinks:load', function() {
+  $('#my-page').on('click', '.card-toggle-btn',function(){
+    let card_block_ele = $(this).closest('.card');
+    let card_body_ele = card_block_ele.children('.card-body');
+    card_body_ele.slideToggle();
+    let card_footer_ele = card_block_ele.children('.card-footer');
+    card_footer_ele.slideToggle();
+    $(this).find('i').toggleClass('fa-toggle-on fa-toggle-off');
+  });
+});
